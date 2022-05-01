d8ca958eebccf7fa89040dd22285cca87fe58329
Andreas Lau Hansen <s194235@student.dtu.dk>
2022-05-01 17:05:53 +0200

    network test

---
+++ static/network/parse_options.js
@@ -0,0 +1,20 @@
+// =========== GET DATA AND CONFIG FROM URL PARAMETERS =================
+controls = {};
+
+var this_url =  window.location.href;
+this_url = new URL(this_url);
+
+var this_data = this_url.searchParams.get("data");
+if (!((this_data == "null") || (this_data === null))) {
+    controls['file_path'] = this_data;
+}
+
+var this_config = this_url.searchParams.get("config");
+if ( !((this_config == "null") || (this_config === null))) {
+    d3.json(this_config, new_controls => {
+    	controls = {...controls, ...new_controls}
+    	vis(controls);
+    })
+} else {
+	vis(controls);
+}
